6e05909d2ff37d45f1bd07217ea11b71de34752a
wei.sun <wei.sun@spreadtrum.com>
2012-08-16 19:03:38 +0800

    nil

--- app/models/deltum.rb
+++ app/models/deltum.rb
@@ -4,19 +4,7 @@ class Deltum < ActiveRecord::Base
   belongs_to :b_version,:class_name=>'Version'
   belongs_to :product
 
-  attr_accessor :a_version_str,:b_version_str,:vender_str
-  attr_accessible :a_version_str,:b_version_str,:vender_str,:path,:size
+  attr_accessible :a_version,:b_version,:product,:path,:size
   validates_presence_of :a_version,:b_version,:path,:size,:product
 
-  def vender_str=(vender_str)
-    self.product=Product.find_by_vender(vender_str)
-  end
-
-  def a_version_str=(version_str)
-    self.a_version=Version.find_by_version(version_str)
-  end
-
-  def b_version_str=(version_str)
-    self.b_version=Version.find_by_version(version_str)
-  end
 end
