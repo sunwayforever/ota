e364b2a1b7307aaa2fb6ef93d3378e010a19c833
wei.sun <wei.sun@spreadtrum.com>
2012-08-16 21:22:57 +0800

    nil

--- app/models/cellular.rb
+++ app/models/cellular.rb
@@ -1,21 +1,9 @@
 class Cellular < ActiveRecord::Base
-  attr_accessible :jid, :product_id, :version_id
+  attr_accessible :jid, :product_id, :version_id, :product,:version
   belongs_to :product
   belongs_to :version
 
   attr_accessible :vender_str,:version_str
   validates_presence_of :product,:version
 
-  def vender_str
-    return self.product.vender if self.product!=nil
-  end
-  def version_str
-    return self.version.version if self.product!=nil
-  end
-  def vender_str=(vender_str)
-    self.product=Product.find_by_vender(vender_str)
-  end
-  def version_str=(version_str)
-    self.version=Version.find_by_version(version_str)
-  end
 end
